f4b39fc1bb0911cf3cb1181e3454ceda748d6e7d
hiyee-gj <gyeongjae.h.dev@gmail.com>
2022-11-01 16:54:40 +0900

    #8 - 회원가입 테스트 작성하기
    회원 가입 테스트를 작성하기 위해서 `User` model 생성
    `UserController`, `UserService` 생성
    request 시 사용할 `UserJoinRequest` 생성
    테스트 코드 실패 경우에 사용할 `SnsApplicationException` 생성

---
+++ src/main/java/com/fastcampus/sns/service/UserService.java
@@ -0,0 +1,13 @@
+package com.fastcampus.sns.service;
+
+import com.fastcampus.sns.model.User;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    // TODO : implement
+    public User join() {
+        return new User();
+    }
+}
---
+++ src/test/java/com/fastcampus/sns/controller/UserControllerTest.java
@@ -0,0 +1,70 @@
+package com.fastcampus.sns.controller;
+
+import com.fastcampus.sns.controller.request.UserJoinRequest;
+import com.fastcampus.sns.exception.SnsApplicationException;
+import com.fastcampus.sns.model.User;
+import com.fastcampus.sns.service.UserService;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class UserControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @MockBean
+    private UserService userService;
+
+    @Test
+    public void 회원가입() throws Exception {
+        // Given
+        String userName = "userName";
+        String password = "password";
+
+        when(userService.join()).thenReturn(mock(User.class));
+
+        // When $ Then
+        mockMvc.perform(post("/api/v1/users/join")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsBytes(new UserJoinRequest(userName, password)))
+                ).andDo(print())
+                .andExpect(status().isOk());
+
+    }
+
+    @Test
+    public void 회원가입시_이미_회원가입된_userName으로_회원가입을_하는경우_에러반환() throws Exception {
+        // Given
+        String userName = "userName";
+        String password = "password";
+
+        when(userService.join()).thenThrow(new SnsApplicationException());
+
+        // When $ Then
+        mockMvc.perform(post("/api/v1/users/join")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsBytes(new UserJoinRequest(userName, password)))
+                ).andDo(print())
+                .andExpect(status().isConflict());
+
+    }
+
+}
